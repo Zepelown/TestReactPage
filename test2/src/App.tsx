50b3cb41638c21485b484cd20cde149bb3df0047
Zepelown <dbswjdqhd12@gmail.com>
2023-02-07 17:43:20 +0900

    init

---
+++ test2/src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import BaseLayout from "./components/layouts/BaseLayout";
+
+import Home from "~/pages/Home";
+import About from "~/pages/About";
+import Test from "~/pages/Test";
+import NotFound from "~/pages/NotFound";
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BaseLayout/>}>
+          <Route index element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/test/:id" element={<Test/>}/>
+        </Route>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ test2/src/pages/Test.tsx
@@ -0,0 +1,9 @@
+import { useParams } from "react-router-dom";
+
+const Test = () => {
+    const {id} = useParams();
+    return <div>현재 위치 "/test/{id}"</div>;
+  };
+  
+  export default Test;
+  
